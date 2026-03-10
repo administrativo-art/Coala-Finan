9a74ab42928d2c908bd3810c9e1d0f8e216beeaa
Coala Shakes <administrativo@coalashakes.com>
2026-03-10 15:11:51 +0000

    feça um resumo do problema da api key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,5 @@
 // As chaves são carregadas a partir de variáveis de ambiente (arquivo .env)
+// Isso impede que segredos sejam expostos no GitHub.
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
